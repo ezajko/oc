0d2e41c6919c35ee10064278c7d49be76249894a
dhilst <danielhilst@gmail.com>
2019-11-04 22:03:11 -0300

    Use new approach for image creation, by appending repositories urls to pkgdir

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
 class FilterModule(object):
     def filters(self):
-        return {"reverse_name": self.reverse_name}
+        return {"reverse_name": self.reverse_name,
+                "nodes_names": self.nodes_names,
+                "node_name": self.node_name,
+                "inc": self.inc,
+                }
 
     def reverse_name(self, ip):
         return ".".join(reversed(ip.split(".")[0:2])) + ".in-addr.arpa."
+
+    def nodes_names(self, nodes_num, nodes_prefix, nodes_separator, nodes_padding):
+        return ['{}{}{:0{prec}}'.format(nodes_prefix, nodes_separator, i, prec=nodes_padding) for i in range(1, nodes_num+1)]
+
+    def node_name(self, i, nodes_prefix, nodes_separator, nodes_padding):
+        return '{}{}{:0{prec}}'.format(nodes_prefix, nodes_separator, i, prec=nodes_padding)
+
+    def inc(self, val):
+        return int(val) + 1
+
